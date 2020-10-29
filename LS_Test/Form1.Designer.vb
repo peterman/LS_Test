dc65dc0e3ecbe811f9e8b23e93f2e2b67e28873c
peter.pfeiffer <peter.pfeiffer@dreebit.com>
2020-10-29 14:40:13 +0100

    import

--- LS_Test/Form1.Designer.vb
+++ LS_Test/Form1.Designer.vb
@@ -22,17 +22,229 @@ Partial Class Main
     'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ComboBox4 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.RadioButton5 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.ComboBox5 = New System.Windows.Forms.ComboBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripStatusLabel2 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripProgressBar1 = New System.Windows.Forms.ToolStripProgressBar()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.ToolStripStatusLabel3 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.StatusStrip1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(124, 72)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(124, 114)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Stop"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ComboBox4
+        '
+        Me.ComboBox4.FormattingEnabled = True
+        Me.ComboBox4.Items.AddRange(New Object() {"1", "2", "3", "4", "5"})
+        Me.ComboBox4.Location = New System.Drawing.Point(308, 257)
+        Me.ComboBox4.Name = "ComboBox4"
+        Me.ComboBox4.Size = New System.Drawing.Size(65, 21)
+        Me.ComboBox4.TabIndex = 22
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Items.AddRange(New Object() {"60", "120", "180", "240", "300"})
+        Me.ComboBox3.Location = New System.Drawing.Point(308, 230)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(65, 21)
+        Me.ComboBox3.TabIndex = 21
+        '
+        'RadioButton5
+        '
+        Me.RadioButton5.AutoSize = True
+        Me.RadioButton5.Location = New System.Drawing.Point(428, 261)
+        Me.RadioButton5.Name = "RadioButton5"
+        Me.RadioButton5.Size = New System.Drawing.Size(40, 17)
+        Me.RadioButton5.TabIndex = 20
+        Me.RadioButton5.TabStop = True
+        Me.RadioButton5.Text = "Cal"
+        Me.RadioButton5.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(428, 234)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(47, 17)
+        Me.RadioButton4.TabIndex = 19
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "Vent"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(428, 206)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(64, 17)
+        Me.RadioButton3.TabIndex = 18
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "Standby"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(428, 177)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(62, 17)
+        Me.RadioButton2.TabIndex = 17
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Messen"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"60", "120", "180", "240", "300"})
+        Me.ComboBox2.Location = New System.Drawing.Point(308, 203)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(65, 21)
+        Me.ComboBox2.TabIndex = 16
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"60", "120", "300", "600"})
+        Me.ComboBox1.Location = New System.Drawing.Point(308, 173)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(65, 21)
+        Me.ComboBox1.TabIndex = 15
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(541, 27)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(232, 321)
+        Me.TextBox3.TabIndex = 23
+        '
+        'ComboBox5
+        '
+        Me.ComboBox5.FormattingEnabled = True
+        Me.ComboBox5.Location = New System.Drawing.Point(255, 58)
+        Me.ComboBox5.Name = "ComboBox5"
+        Me.ComboBox5.Size = New System.Drawing.Size(85, 21)
+        Me.ComboBox5.TabIndex = 25
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(255, 91)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(46, 20)
+        Me.TextBox6.TabIndex = 24
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1, Me.ToolStripStatusLabel2, Me.ToolStripProgressBar1, Me.ToolStripStatusLabel3})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 428)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(800, 22)
+        Me.StatusStrip1.TabIndex = 26
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(119, 17)
+        Me.ToolStripStatusLabel1.Text = "ToolStripStatusLabel1"
+        '
+        'ToolStripStatusLabel2
+        '
+        Me.ToolStripStatusLabel2.Name = "ToolStripStatusLabel2"
+        Me.ToolStripStatusLabel2.Size = New System.Drawing.Size(119, 17)
+        Me.ToolStripStatusLabel2.Text = "ToolStripStatusLabel2"
+        '
+        'ToolStripProgressBar1
+        '
+        Me.ToolStripProgressBar1.Name = "ToolStripProgressBar1"
+        Me.ToolStripProgressBar1.Size = New System.Drawing.Size(100, 16)
+        '
+        'ToolStripStatusLabel3
+        '
+        Me.ToolStripStatusLabel3.Name = "ToolStripStatusLabel3"
+        Me.ToolStripStatusLabel3.Size = New System.Drawing.Size(119, 17)
+        Me.ToolStripStatusLabel3.Text = "ToolStripStatusLabel3"
         '
         'Main
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.ComboBox5)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.ComboBox4)
+        Me.Controls.Add(Me.ComboBox3)
+        Me.Controls.Add(Me.RadioButton5)
+        Me.Controls.Add(Me.RadioButton4)
+        Me.Controls.Add(Me.RadioButton3)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
         Me.Name = "Main"
         Me.Text = "Lecksucher-Testlauf"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ComboBox4 As ComboBox
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents RadioButton5 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents ComboBox5 As ComboBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusLabel2 As ToolStripStatusLabel
+    Friend WithEvents ToolStripProgressBar1 As ToolStripProgressBar
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents ToolStripStatusLabel3 As ToolStripStatusLabel
 End Class
--- LS_Test/Form1.vb
+++ LS_Test/Form1.vb
@@ -1,5 +1,152 @@
-﻿Public Class Main
+﻿Imports System
+Imports System.IO
+Imports System.Collections
+
+Public Class Main
+
+    Dim Testcycle,
+        Teststep,
+        Testcount,
+        Testcountmax As Integer
+    Dim SerPort As Array
+
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Splash.Show()
+
+        Init()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Test_start()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Test_stop()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Select Case Teststep
+            Case 1                                  'Betriebsart Messen
+                RadioButton2.Checked = True
+                If Testcount = 0 Then
+                    Teststep = 2
+                    Testcount = Val(ComboBox2.SelectedItem)
+                    Testcountmax = Testcount
+                    command("Stop")
+                End If
+            Case 2                                  'Betriebsart Standby ohne Vent
+                RadioButton3.Checked = True
+                If Testcount = 0 Then
+                    Teststep = 3
+                    Testcount = Val(ComboBox3.SelectedItem)
+                    Testcountmax = Testcount
+                    command("Vent")
+                End If
+            Case 3                                  'Betriebsart Vent
+                RadioButton4.Checked = True
+                If Testcount = 0 Then
+                    If Testcycle = 0 Then
+                        Teststep = 4
+                        Testcount = Val(ComboBox4.SelectedItem)
+                        Testcountmax = Testcount
+                        command("Cal")
+                    Else
+                        Teststep = 1
+                        Testcount = Val(ComboBox1.SelectedItem)
+                        Testcountmax = Testcount
+                        Testcycle -= 1
+                        command("start")
+                    End If
+                End If
+            Case 4
+                RadioButton5.Checked = True
+                If Testcount = 0 Then
+                    Teststep = 1
+                    Testcount = Val(ComboBox1.SelectedItem)
+                    Testcountmax = Testcount
+                    command("start")
+                End If
+        End Select
+        ToolStripProgressBar1.Value = (Testcount / Testcountmax) * 100
+        Testcount -= 1
+        TextBox6.Text = Testcount
 
     End Sub
+
+    Private Sub Init()
+        ComboBox1.SelectedIndex = 0
+        ComboBox2.SelectedIndex = 0
+        ComboBox3.SelectedIndex = 0
+        ComboBox4.SelectedIndex = 0
+        Timer1.Interval = 100
+        SerPort = IO.Ports.SerialPort.GetPortNames
+        ComboBox5.Items.AddRange(SerPort)
+    End Sub
+
+    Private Sub Test_start()
+        ComboBox1.Enabled = False
+        ComboBox2.Enabled = False
+        ComboBox3.Enabled = False
+        ComboBox4.Enabled = False
+        SerialPort1.PortName = ComboBox1.Text
+        SerialPort1.BaudRate = 9600
+        SerialPort1.Parity = IO.Ports.Parity.None
+        SerialPort1.StopBits = IO.Ports.StopBits.One
+        SerialPort1.DataBits = 8
+        SerialPort1.Open()
+        ComboBox5.Enabled = False
+        Teststep = 1
+        Testcycle = 1
+        Testcount = Val(ComboBox1.SelectedItem)
+        Testcountmax = Testcount
+
+        Timer1.Enabled = True
+        Timer1.Start()
+
+        TextBox6.Text = Testcount
+        command("start")
+    End Sub
+
+    Private Sub Test_stop()
+        Timer1.Enabled = False
+        ComboBox1.Enabled = True
+        ComboBox2.Enabled = True
+        ComboBox3.Enabled = True
+        ComboBox4.Enabled = True
+        ComboBox5.Enabled = True
+        Timer1.Stop()
+
+    End Sub
+
+    Private Sub command(cmd As String)
+        Select Case cmd.ToLower
+
+            Case "start"
+                ToolStripStatusLabel3.Text = "Start Messen"
+            Case "stop"
+                ToolStripStatusLabel3.Text = "Stop Messen"
+            Case "cal"
+                ToolStripStatusLabel3.Text = "Start Kalibrieren"
+            Case "vent"
+                ToolStripStatusLabel3.Text = "Belüften"
+        End Select
+    End Sub
+
+    Private Sub SendRS232(cmd As String)
+        Select Case cmd.ToLower
+            Case "start"
+                'writeRS232 = "=CYE" & vbcr
+            Case "stop"
+                'writeRS232 = "=CYD" & vbcr
+            Case "cal"
+                'writeRS232 = "!AC" & vbcr
+            Case "vent"
+                'writeRS232 = "=IVE" & vbcr
+            Case "status"
+                'writeRS232 = "?TR" & vbcr
+        End Select
+    End Sub
+
+
 End Class
